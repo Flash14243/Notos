cb409758107a6d12f41316063b5ff478d3bf5a89
Flash14243 <sklyarov345@gmail.com>
2020-11-29 13:15:03 +0500

    Половина ненужных файлов удалена, начальный экран изменён на почту, начата работа над профилем

---
+++ auth/app/src/main/java/com/google/firebase/quickstart/auth/java/ProfileActivity.java
@@ -0,0 +1,10 @@
+package com.google.firebase.quickstart.auth.java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+
+}
